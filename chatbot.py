13eb1008a628e75edf3b7b5533f65c1b16d80568
Aryan Mathur <126969847+neverthesameagain@users.noreply.github.com>
2023-05-28 18:17:45 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,6 @@
 #homepage
+#using NLP NOW> 
+refer this.
+https://github.com/simranjeet97/ChatBot_Tensorflow_NLP/blob/master/Contextual%20Chatbot%20-%20NLP%20and%20Tensorflow.ipynb
+  and this 
+  https://github.com/simranjeet97/ChatBot_Tensorflow_NLP/blob/master/Chatterbot.py
